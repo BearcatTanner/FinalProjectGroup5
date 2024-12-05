7b416a6e71e8ef403969f0f289e0383dedd61278
BearcatTanner <Tannerdpatrick06@gmail.com>
2024-12-05 12:44:13 -0500

    added migration to hobbies table

---
+++ FinalProjectGroup5/Migrations/20241205173659_hobbiesMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProjectGroup5.Migrations
+{
+    /// <inheritdoc />
+    public partial class hobbiesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hobbies",
+                columns: table => new
+                {
+                    HobbyId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    HoursPerWeek = table.Column<int>(type: "int", nullable: false),
+                    PreferredPlatform = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hobbies", x => x.HobbyId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hobbies");
+        }
+    }
+}
